1507470a5346718588fcf106a2b7775e315f36fb
kbub27 <kalebmerriman27@gmail.com>
2019-04-17 18:34:38 -0700

    got more than just one marker to show up now just to recenter the map

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -15,6 +15,14 @@ var map;
 var marker;
 var lat =  47.62541904760501;
 var long = -152.33551025390625;
+var lat1 = 0;
+var lat2 = 0;
+var lat3 = 0;
+var lat4 = 0;
+var long1 = 0;
+var long2 = 0;
+var long3 = 0;
+var long4 = 0;
 var businessName = '';
 
 function initMap() {
@@ -23,8 +31,6 @@ function initMap() {
         center: { lat: 47.62541904760501, lng: -122.33551025390625 },
         zoom: 8
     });
-    
-    // To add the marker to the map, call setMap();
 }
 
 
@@ -41,31 +47,60 @@ database.ref().child('/yelp/businesses/0').on('value', function (snap) {
     marker.setMap(map);
     
 });
-// database.ref().child('/yelp/businesses/1').on('value', function (snap) {
-//     console.log(snap.val());
-//     lat = snap.val().coordinates.latitude;
-//     long = snap.val().coordinates.longitude;
-//     businessName = snap.val().alias;
-//     console.log(lat, long, businessName);
-// });
-// database.ref().child('/yelp/businesses/2').on('value', function (snap) {
-//     console.log(snap.val());
-//     lat = snap.val().coordinates.latitude;
-//     long = snap.val().coordinates.longitude;
-//     businessName = snap.val().alias;
-//     console.log(lat, long, businessName);
-// });
-// database.ref().child('/yelp/businesses/3').on('value', function (snap) {
-//     console.log(snap.val());
-//     lat = snap.val().coordinates.latitude;
-//     long = snap.val().coordinates.longitude;
-//     businessName = snap.val().alias;
-//     console.log(lat, long, businessName);
-// });
-// database.ref().child('/yelp/businesses/4').on('value', function (snap) {
-//     console.log(snap.val());
-//     lat = snap.val().coordinates.latitude;
-//     long = snap.val().coordinates.longitude;
-//     businessName = snap.val().alias;
-//     console.log(lat, long, businessName);
-// });
+
+database.ref().child('/yelp/businesses/1').on('value', function (snap) {
+    console.log(snap.val());
+    lat1 = snap.val().coordinates.latitude;
+    long1 = snap.val().coordinates.longitude;
+    businessName = snap.val().alias;
+    console.log(lat, long, businessName);
+    marker = new google.maps.Marker({
+        position: {lat: lat1, lng: long1},
+        title: businessName
+    });
+    marker.setMap(map);
+    
+});
+
+database.ref().child('/yelp/businesses/2').on('value', function (snap) {
+    console.log(snap.val());
+    lat2 = snap.val().coordinates.latitude;
+    long2 = snap.val().coordinates.longitude;
+    businessName = snap.val().alias;
+    console.log(lat, long, businessName);
+    marker = new google.maps.Marker({
+        position: {lat: lat2, lng: long2},
+        title: businessName
+    });
+    marker.setMap(map);
+    
+});
+
+database.ref().child('/yelp/businesses/3').on('value', function (snap) {
+    console.log(snap.val());
+    lat3 = snap.val().coordinates.latitude;
+    long3 = snap.val().coordinates.longitude;
+    businessName = snap.val().alias;
+    console.log(lat, long, businessName);
+    marker = new google.maps.Marker({
+        position: {lat: lat3, lng: long3},
+        title: businessName
+    });
+    marker.setMap(map);
+    
+});
+
+database.ref().child('/yelp/businesses/4').on('value', function (snap) {
+    console.log(snap.val());
+    lat4 = snap.val().coordinates.latitude;
+    long4 = snap.val().coordinates.longitude;
+    businessName = snap.val().alias;
+    console.log(lat, long, businessName);
+    marker = new google.maps.Marker({
+        position: {lat: lat4, lng: long4},
+        title: businessName
+    });
+    marker.setMap(map);
+    
+});
+
